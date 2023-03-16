a2099abdfeaf3034da204dca91c916af4410f20f
dili <di.grebenev@outlook.com>
2023-03-16 16:50:59 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,29 @@ function App() {
   // const API_KEY = `${API_KEY}`
 
   const locations = [{ id: 1, title: 'msk', subscrib: 'Москва', lan: '55.44', lot: '37.36' },
-  { id: 1, title: 'spb', subscrib: 'Санкт-Петербург', lan: '59,56', lot: '30,19' },
-  { id: 1, title: 'ekb', subscrib: 'Екатеринбург', lan: '50.56', lot: '60.35' },]
+  { id: 2, title: 'spb', subscrib: 'Санкт-Петербург', lan: '59,56', lot: '30,19' },
+  { id: 3, title: 'ekb', subscrib: 'Екатеринбург', lan: '50.56', lot: '60.35' },]
 
-  const[value, setValue] = useState('')
+  const [value, setValue] = useState('')
+  
+  console.log(locations.find(item=> item.subscrib === value).subscrib)
 
   return (
 
     <div className="App">
-      <h1 >weather</h1>
+      <h1 style={{textAlign:'center'}}>weather </h1>
       <hr style={{ margin: '15px 0' }} />
       <SelectItem
         options={locations}
         defaultValue='Город'
         value={value}
         onChange={setValue}
-      />
-      {/* location.find(id=>locaition.id==) */}
-      <h1>{value}</h1>
+      />   
+    {/* {value}
+    {locations.find(item=> item.subscrib === value).lan} */}
+
+
+
     </div>
   );
 }
--- src/components/UI/select/SelectItem.jsx
+++ src/components/UI/select/SelectItem.jsx
@@ -3,15 +3,13 @@ import classes from './SelectItem.module.css'
 
 
 
-const SelectItem = ({ options, defaultValue, value, onChange }) => {
+const SelectItem = ({options, defaultValue, value, onChange}) => {
     return (
-        <select
-            value={value}
-            onChange ={event => onChange(event.target.value)}
-        >
+        <select value={value} onChange={event => onChange(event.target.value)}>
+
             <option disabled value="">{defaultValue}</option>
             {options.map(option =>
-                <option key={option.id} value={option.title}>
+                <option key={option.id} item={option} >
                     {option.subscrib}
                 </option>
             )}
